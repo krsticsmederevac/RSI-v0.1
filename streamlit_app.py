dfee019228f3d001ad7cd5ad6269180c396b79b7
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-01 23:53:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,6 +104,8 @@ source = ColumnDataSource(data=dict(
 
 group = dt['coin']
 
+rsi_tf = "RSI " + intervali[0]
+
 p = figure(x_range=group, y_range=(0,101),#height=600,width=1200,  
            title="RSI")
 
