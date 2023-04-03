ffcec130560f0388528e7639ed7fced94c12477a
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-03 14:11:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -219,7 +219,7 @@ if usdt_btc and kolona_sortiranja:
         
         container.bokeh_chart(p)
         
-st.download_button("Download Coin List",json_podesavanja,"coin_setup.json","application/json")
+container.download_button("Download Coin List",json_podesavanja,"coin_setup.json","application/json")
 
 st.write("Feel free to leave a tip.")
 st.write("Eth: 0xe183bf9861b995107df580e1f9fa2a5e56e9ea40")
