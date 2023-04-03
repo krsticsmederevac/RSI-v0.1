cafc1e3546da99ba1c9aeb41d785a4b7cf90ad51
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-03 13:29:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -170,7 +170,7 @@ if podesavanja_korisnika:
     pocetni_simboli = []
     
     for key in podesavanja_korisnika_recnik['simboli']:
-        pocetni_simboli.append(key.value())
+        pocetni_simboli.append(key.values())
             
     
     st.write(pocetni_simboli)
