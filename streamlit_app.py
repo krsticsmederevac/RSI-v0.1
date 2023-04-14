b1d3cdface72738146ef9280a5baa076c5bce8f5
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-14 23:39:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -225,9 +225,9 @@ if usdt_btc and kolona_sortiranja:
       
         dt = data_frame_maker(simboli, interval, oscilator, izbor_usdt_btc, kolona_sortiranja)
         p = grafik_oscilator_interval(dt,interval,oscilator,izbor_usdt_btc)
-        dt.style.background_gradient(axis=0, vmin=0,vmax = 100)
+        stil = dt.style.background_gradient(axis=0, gmap = df['rsi'], vmin=0,vmax = 100)
         container.bokeh_chart(p)
-        container.dataframe(dt)
+        container.dataframe(stil)
         
 container.download_button("Download Coin List",json_podesavanja,"my_coin_list.json","application/json")
 
