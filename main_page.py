5e4c47c485958ed6d180831543d4e2561bc3abe3
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-18 02:58:26 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-def streamlit_menu():
-   selected = option_menu(
+selected = option_menu(
               menu_title=None,  # required
               options=["RSI Field", "Scaner"],  # required
               icons=["house", "book", ],  # optional
@@ -10,7 +9,6 @@ def streamlit_menu():
               default_index=0,  # optional
               orientation="horizontal"
           )
-   return selected
  
  
-selected = streamlit_menu()
+selected
