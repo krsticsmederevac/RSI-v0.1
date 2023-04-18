081e72a323b05b5d0ecf64c70b6b27aa0514650c
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-18 02:55:07 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -4,7 +4,7 @@ from streamlit_option_menu import option_menu
 def streamlit_menu():
    selected = option_menu(
               menu_title=None,  # required
-              options=["RSI", "Scaner"],  # required
+              options=["RSI Field", "Scaner"],  # required
               icons=["house", "book", ],  # optional
               menu_icon="cast",  # optional
               default_index=0,  # optional
