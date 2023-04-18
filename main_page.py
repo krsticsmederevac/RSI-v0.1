6aa0a8f000c381ba8c9a2458d31cee8b21c9ffa3
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-18 02:56:46 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -8,18 +8,7 @@ def streamlit_menu():
               icons=["house", "book", ],  # optional
               menu_icon="cast",  # optional
               default_index=0,  # optional
-              orientation="horizontal",
-              styles={
-                  "container": {"padding": "0!important", "background-color": "#fafafa"},
-                  "icon": {"color": "orange", "font-size": "25px"},
-                  "nav-link": {
-                      "font-size": "25px",
-                      "text-align": "left",
-                      "margin": "0px",
-                      "--hover-color": "#eee",
-                  },
-                  "nav-link-selected": {"background-color": "green"},
-              },
+              orientation="horizontal"
           )
    return selected
  
