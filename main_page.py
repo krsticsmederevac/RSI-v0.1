f9664f8ebf6af2406beeb140e4ee59fa153aa61d
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-21 03:02:30 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -121,7 +121,7 @@ def grafik_oscilator_interval(dt,interval,oscilator,usdt_btc):
     
     prosecan_rsi = dt[oscilator].mean()
     
-    prosek_rsi = Label(x=len(dt)//2, y=17, text='Mean: ' + str(prosecan_rsi)[:4], text_color = 'orange' ,text_font_size = '12pt' )
+    prosek_rsi = Label(x=len(dt)//2, y=17, text='Mean: ' + str(prosecan_rsi)[:4], text_color = 'green' ,text_font_size = '12pt' )
 
     p.add_layout(prosek_rsi)
 
