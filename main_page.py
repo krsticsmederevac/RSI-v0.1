179fdf724a29c154643e07ed306d2b0b9bd23187
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-04-27 00:50:38 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config( page_title = "RSI Field", page_icon = "🧰",layout="wide",initial_sidebar_state="auto", menu_items = {"About": "krsticsmederevac@gmail.com"})
+st.set_page_config( page_title = "RSI Field", page_icon = ":bitcoin:",layout="wide",initial_sidebar_state="auto", menu_items = {"About": "krsticsmederevac@gmail.com"})
 
 import extra_streamlit_components as stx
 
