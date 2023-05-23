b4601d8f0c867226605580246a05b7f58fdd5bee
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-24 00:24:14 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -249,8 +249,7 @@ import extra_streamlit_components as stx
         
 # container.download_button("Download Coin List",json_podesavanja,"my_coin_list.json","application/json")
 url = "https://www.tradingview.com/script/48BzLTIt-CryptoData-Coin-BTC-Quick-Chart-ic/"
-st.write("check out this [link](%s)" % url)
-st.markdown("check out this [link](%s)" % url)
+st.write("Check out my TradingView indicators [link](%s)" % url)
 st.write("Feel free to leave a tip.")
 st.write("Eth: 0xb77fcef5c13e1a54bdfff9a7e5268743e81325c4")
 st.write("Tron: TVT4GcBP29NoiuHTttfa4QJA837rv9XZ7v")
