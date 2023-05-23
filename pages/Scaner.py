132ff8069758ff57668063e827c257793d10ecfa
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-23 22:23:48 +0200

    Update Scaner.py

--- pages/Scaner.py
+++ pages/Scaner.py
@@ -192,14 +192,14 @@ if usdt_btc :
     
     
     
-#     dt['Change'] = round(dt['change'],2).astype(str) + '%'
+    dt['Change'] = round(dt['change'],2).astype(str) + '%'
     dt.RSI = round(dt[['RSI']],1).astype(str)
     dt.change = round(dt[['change']],2)
     
-    dt1 = dt[['RSI','change']]
+    dt1 = dt[['RSI','Change']]
    
-    dt1 = dt1.style.background_gradient(cmap = 'RdYlGn',subset = ['RSI','change'] )
-    dt1['change'] = round(dt1['change'],2).astype(str) + '%'
+    dt1 = dt1.style.background_gradient(cmap = 'RdYlGn',subset = ['RSI','Change'] )
+    
     
     with tab1:
         tab1.dataframe(dt1,use_container_width= False)
