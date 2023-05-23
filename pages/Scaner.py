719a2292bca875917d293332fd372c69603dab98
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-23 23:06:01 +0200

    Update Scaner.py

--- pages/Scaner.py
+++ pages/Scaner.py
@@ -202,7 +202,7 @@ if usdt_btc :
     
     
     dt1 = dt1.style.background_gradient(cmap = 'RdYlGn',subset = ['RSI'] )
-    dt2 = dt2.style.background_gradient(subset=['change'], cmap = 'viridis')
+    dt2 = dt2.style.background_gradient(subset=['change'], cmap = 'Greens')
     
     with tab1:
         tab1.dataframe(dt1,use_container_width= False)
