dddae8759b8ba314ae98fa79457c4642ad323056
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-26 14:19:26 +0200

    Update RSI.py

--- pages/RSI.py
+++ pages/RSI.py
@@ -250,8 +250,9 @@ if usdt_btc and kolona_sortiranja:
         dt = data_frame_maker(simboli, interval, oscilator, izbor_usdt_btc, kolona_sortiranja)
         p = grafik_oscilator_interval(dt,interval,oscilator,izbor_usdt_btc)
         dt = dt.set_index('coin')
-        dt.rsi = round(dt[['rsi']],1)
-        stil = dt.style.background_gradient(axis=0, cmap = 'RdYlGn')
+        dt.rsi = round(dt[['rsi']],1).astype(str)
+       
+        stil = dt.style.background_gradient(axis=0, cmap = 'RdYlGn' subset=['RSI'] )
         if 'Chart' in chart_table:
             with tab1:
                 tab1.bokeh_chart(p)
