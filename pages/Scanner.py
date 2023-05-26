7c7cc88438a745e87d0744e5d67f7de736328019
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-26 22:02:26 +0200

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -185,7 +185,7 @@ if usdt_btc :
     dt1 = dt.pivot(index='coin', columns='timeframe', values='RSI')
     dt1.style.background_gradient(cmap ='RdYlGn')
     
-    fig, ax = plt.subplots(figsize = (6, 25))
+    fig, ax = plt.subplots()
     sns.heatmap(dt1, cmap ='RdYlGn',vmin=-0, vmax=100,  linewidths = 0.30, annot = True, cbar=False).set_title("RSI")
     ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
     
