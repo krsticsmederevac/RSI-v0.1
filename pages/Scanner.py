9546f64ef48f723df48ea57fe98be9c364b1b67f
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-27 02:13:32 +0200

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -243,7 +243,7 @@ if usdt_btc :
         ema_sma_size = len(interval)/2
         fig4, ax4 = plt.subplots(figsize = (ema_sma_size,fig_high))
         sns.heatmap(dt_sma, cmap ='RdYlGn',vmin=-1, vmax=1,  linewidths = 0.30, annot = False, cbar=False).set_title("SMA 10 20 100 200")
-        ax4.tick_params(top=True, labeltop=True, bottom=False, labelbottom=True)
+        ax4.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
         ax4.set_xticklabels(ax4.get_xticklabels(), rotation=90, ha='center',horizontalalignment="center")
         ax4.set_xlabel('SMA')
 
