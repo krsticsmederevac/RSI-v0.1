8b40a4486161b7644d23de0ba62951d6af1c4dbf
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-27 03:08:25 +0200

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -201,12 +201,16 @@ if usdt_btc :
         fig1, ax1 = plt.subplots(figsize = (1.5,fig_high))
         sns.heatmap(dt1, cmap ='RdYlGn',vmin=0, vmax=100,  linewidths = 0.30, annot = True, cbar=False).set_title("RSI")
         ax1.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+        ax1.set_xticklabels(ax1.get_xticklabels(), rotation=90, ha='center')
+        ax1.set_yticklabels(ax1.get_yticklabels(), rotation=0, ha='center')
         ax1.set_xlabel('')
         ax1.set_ylabel('')
         
         fig2, ax2 = plt.subplots(figsize = (1.5,fig_high))
         sns.heatmap(dt2, cmap ='RdYlGn',vmin=-3, vmax=3,  linewidths = 0.30, annot = True, cbar=False).set_title("Price Change %")
         ax2.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+        ax2.set_xticklabels(ax2.get_xticklabels(), rotation=90, ha='center')
+        ax2.set_yticklabels(ax2.get_yticklabels(), rotation=0, ha='center')
         ax2.set_xlabel('')
         ax2.set_ylabel('')
         
@@ -252,6 +256,7 @@ if usdt_btc :
         sns.heatmap(dt_sma, cmap ='RdYlGn',vmin=-1, vmax=1,  linewidths = 0.30, annot = False, cbar=False).set_title("SMA 10 20 100 200")
         ax4.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
         ax4.set_xticklabels(ax4.get_xticklabels(), rotation=90, ha='center')
+        ax4.set_yticklabels(ax4.get_yticklabels(), rotation=0, ha='center')
         ax4.set_xlabel('')
         ax4.set_ylabel('')
 
