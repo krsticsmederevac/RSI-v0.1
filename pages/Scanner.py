cbf3b9b0b5104a1c0b46fa9b8fd10c0f981c3940
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-27 22:30:19 +0200

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -196,10 +196,10 @@ if usdt_btc :
 
         
         fig_high = len(dt.index ) / 20
-        sns.set(font_scale=0.3)
+        sns.set(font_scale=0.4)
         
         fig1, ax1 = plt.subplots(figsize = (1.5,fig_high))
-        sns.heatmap(dt1, cmap ='RdYlGn',vmin=0, vmax=100,  linewidths = 0.30, annot = True, cbar=False).set_title("RSI")
+        sns.heatmap(dt1, cmap ='RdYlGn',vmin=0, vmax=100,  linewidths = 0.30, annot = True, cbar=False,square=True).set_title("RSI")
         ax1.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
         ax1.set_xticklabels(ax1.get_xticklabels(), rotation=90, ha='center')
         ax1.set_yticklabels(ax1.get_yticklabels(), rotation=0, ha='center')
