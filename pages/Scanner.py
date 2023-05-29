4c14bb4599f7c710b7acb5df56ded9bcbdaf5837
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-05-29 16:52:12 +0200

    Update Scanner.py

--- pages/Scanner.py
+++ pages/Scanner.py
@@ -274,14 +274,14 @@ if usdt_btc :
 #         ((dt['BB.upper']> dt.high) | (dt['BB.upper']> dt.close) | (dt['BB.lower']< dt.low) | (dt['BB.lower']<dt.close))
         ]
         
-        values = [np.nan, 1,-1, 0.5, -0.7]
+        values = [np.nan, 1,-1, 0.5, -0.9]
 
         dt['BB'] =np.select(conditions,values)
         
         dt5 = dt.pivot(index='coin', columns='timeframe', values='BB')
         
         fig5, ax5 = plt.subplots(figsize = (1.5,fig_high))
-        sns.heatmap(dt5, cmap ='RdYlGn',vmin=-1.5, vmax=1.5,  linewidths = 0.30, annot = False, cbar=False).set_title("Bollinger Bands Hit")
+        sns.heatmap(dt5, cmap ='RdYlGn',vmin=-1.4, vmax=1.4,  linewidths = 0.30, annot = False, cbar=False).set_title("Bollinger Bands Hit")
         ax5.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
         ax5.set_xticklabels(ax5.get_xticklabels(), rotation=90, ha='center')
         ax5.set_yticklabels(ax5.get_yticklabels(), rotation=0, ha='center')
