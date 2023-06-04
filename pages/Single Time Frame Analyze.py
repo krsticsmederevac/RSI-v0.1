87fe10d1fc74f2504a2cf7c2d223cceff54e6811
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-05 01:12:44 +0200

    Update Single Time Frame Analyze.py

--- pages/Single Time Frame Analyze.py
+++ pages/Single Time Frame Analyze.py
@@ -187,12 +187,12 @@ def grafik_oscilator_interval_sp(dt,interval,oscilator,usdt_btc,sort=True):
     ime_nastavak = '\nMean: ' + str(prosecan_oscilator) + '  Median: ' + str(mediana_oscilator) + '  STD: ' + str(std_oscilator)
     ime_grafika = ime_grafika_osnovno + ime_nastavak
  
-    p = figure(y_range=dt['coin'],x_range =(min(dt[oscilator])*1.05,max(dt[oscilator])*1.05),width=350,height=600,  
+    p = figure(y_range=dt['coin'],x_range =(min(dt[oscilator])*1.05,max(dt[oscilator])*1.05),#width=350,height=600,  
                title = ime_grafika, toolbar_location=None)
 
     p.title.align = 'center'
 
-#     p.sizing_mode = 'scale_width'
+    p.sizing_mode = 'stretch_width'
 #     "fixed", "stretch_both", "scale_width", "scale_height", "scale_both"
     
     
