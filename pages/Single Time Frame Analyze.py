fda50e1eccded833377c0e3b6e86283ffb6f00de
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-05 00:31:13 +0200

    Update Single Time Frame Analyze.py

--- pages/Single Time Frame Analyze.py
+++ pages/Single Time Frame Analyze.py
@@ -5,6 +5,7 @@ import extra_streamlit_components as stx
 
 from tradingview_ta import TA_Handler, Interval, Exchange
 
+import numpy as np
 import time
 import pandas as pd
 import json 
@@ -17,44 +18,61 @@ from bokeh.transform import linear_cmap
 from bokeh.palettes import  RdYlGn
 
 
-def data_frame_maker(simboli, interval, oscilator, usdt_btc, kolona_sortiranja, menjacnica = 'binance'):                        
-                         
+def data_frame_maker(simboli, intervali, analitike, usdt_btc, kolona_sortiranja, menjacnica = 'binance'):                        
+    
+    #ako je izabran par sa BTC ili USDT da dodaje na simbole nastavak
     usdt_ili_btc_lista =  [i + usdt_btc for i in simboli]  
     
+    #u slucaju da ne pronadje par da isece nastavak u obavestenju
     duzina_usdt_ili_btc = len(usdt_btc)
 
-    lista_handler = []
 
-    recnik = {'coin': [], oscilator : []}
+
+    recnik = {'coin': [], 'timeframe' : []}
+    
+    for analitika in analitike:
+        recnik[analitika] = []
+        
 
     for coin in usdt_ili_btc_lista:
+        for interval in intervali:
+            
 
-        handler = TA_Handler(
-            symbol = coin,
-            exchange = menjacnica,
-            screener = "crypto",
-            interval = interval,
-            timeout=None
-            )
+            handler = TA_Handler(
+                symbol = coin,
+                exchange = menjacnica,
+                screener = "crypto",
+                interval = interval,
+                timeout=None
+                )
 
-        try:
-            analysis = handler.get_analysis()
-        except: 
-            print(coin[:-duzina_usdt_ili_btc] + " can't find on exchenge.")
+            try:
+                analysis = handler.get_analysis()
+            except: 
+                print(coin[:-duzina_usdt_ili_btc] + " can't find on exchenge.")
 
-        try:
-            recnik[oscilator].append(round(analysis.indicators[oscilator.upper()],2))
+            
             recnik['coin'].append(coin[:-duzina_usdt_ili_btc])
-        except:
+            recnik['timeframe'].append(interval)
+            try:
+                for analitika in analitike:
+                    recnik[analitika].append(analysis.indicators[analitika])
+            except:
+                for analitika in analitike:
+                    recnik[analitika].append(None)    
                 print(coin[:-duzina_usdt_ili_btc] + "  can't find older data.")    
-        else:continue
+            else:continue
+   
 
 
-        lista_handler.append(handler)
 
     dt = pd.DataFrame(recnik) 
 
-    dt.sort_values(by=[kolona_sortiranja], inplace=True)
+#     dt.sort_values(by=kolona_sortiranja, inplace=True)
+    dt.sort_values(by = 'timeframe', 
+                   key= lambda x: x.map({"1m":1, "5m":2, "15m":3, "30m":4, "1h":5, "2h":6,
+                                         "4h":7, "1d":8,"1W":9, "1M":10})
+                   ,inplace = True)
 
     if len(dt.index) == 0:
         dt = pd.DataFrame({'coin': ["No Data"], oscilator : [0]})
@@ -64,84 +82,184 @@ def data_frame_maker(simboli, interval, oscilator, usdt_btc, kolona_sortiranja, 
   
   
 
-def grafik_oscilator_interval(dt,interval,oscilator,usdt_btc):
+def grafik_oscilator_interval_pc(dt,interval,oscilator,usdt_btc,sort=True):
+    
+    if sort:
+        dt = dt.sort_values(by=[oscilator])
 
     source = ColumnDataSource(data=dict(
         coin=dt['coin'], 
-        rsi=dt[oscilator],
+        indikator = dt[oscilator],
         names=dt['coin']))
+    
+    prosecan_oscilator = round(dt[oscilator].mean(),2)
+    mediana_oscilator = round(dt[oscilator].median(),2)
+    std_oscilator = round(dt[oscilator].std(),2)
 
-
-    group = dt['coin']
-
-    ime_grafika = oscilator.upper() + ' ' + interval + " " + usdt_btc
-
-    p = figure(x_range=group, y_range=(0,101),#height=600,width=1200,  
-               title = ime_grafika, toolbar_location="above")
+    ime_grafika_osnovno = oscilator.upper() + ' ' + interval + ' ' + usdt_btc 
+    ime_nastavak = '\nMean: ' + str(prosecan_oscilator) + '  Median: ' + str(mediana_oscilator) + '  STD: ' + str(std_oscilator)
+    ime_grafika = ime_grafika_osnovno + ime_nastavak
+ 
+    p = figure(x_range=dt['coin'], y_range=(min(dt[oscilator])*1.05,max(dt[oscilator])*1.05),#height=600,width=1200,  
+               title = ime_grafika, toolbar_location=None)
 
     p.title.align = 'center'
 
-    p.sizing_mode='stretch_both'
-
-    upper = BoxAnnotation(bottom=80, fill_alpha=0.1, fill_color='green')
-    p.add_layout(upper)
-
-    upper = BoxAnnotation(bottom=60, fill_alpha=0.1, fill_color='olive')
-    p.add_layout(upper)
-
-    lower = BoxAnnotation(top=40, fill_alpha=0.1, fill_color='red')
-    p.add_layout(lower)
-
-    lower = BoxAnnotation(top=20, fill_alpha=0.1, fill_color='firebrick')
-    p.add_layout(lower)
+    p.sizing_mode = 'stretch_both'
+    
+    
+    
 
 
     palette = RdYlGn[10]
-    color_mapper = linear_cmap(field_name = oscilator, 
+    color_mapper = linear_cmap(field_name = 'indikator', 
                                palette = palette, low = dt[oscilator].max(), 
                                high = dt[oscilator].min())
 
-    p.scatter(x='coin', y=oscilator, size=8, source=source, fill_color=color_mapper)
+    p.scatter(x='coin', y='indikator', size=7, source=source, fill_color=color_mapper)
+    
     p.xaxis.axis_label = 'Coin'
     p.yaxis.axis_label = oscilator.upper() 
 
-    labels = LabelSet(x='coin', y=oscilator, 
-                      text='names',text_alpha = 0.9,text_font_size = '6pt',
-                      text_align = 'right',
+    labels = LabelSet(x='coin', y='indikator', 
+                      text='names',text_alpha = 0.9,text_font_size = '5pt',
+                      text_align = 'center',
                       level='glyph',
-                      x_offset = 10, y_offset = 10,
+                      x_offset = 0, y_offset = 6,
                       source=source)
-    p.xaxis.major_label_orientation = 1.2
-
-
-    polovina = Span(location=50,
-                     line_color='orange',line_dash='dashed', line_width=1)
-    p.add_layout(polovina)
-    p.y_range = Range1d(15,85,bounds=(0, 100))
-    
-    prosecan_rsi = dt[oscilator].mean()
-    mediana_rsi = dt[oscilator].median()
-    
-    prosek_rsi = Label(x=len(dt)//2 - 4, y=17, text='Mean: ' + str(prosecan_rsi)[:4] +'  Median: ' + str(mediana_rsi)[:4], text_color = 'green' ,text_font_size = '12pt' )
-
-    p.add_layout(prosek_rsi)
-
-    hovertool_oscilator = "@" + oscilator
-    p.add_tools(HoverTool(tooltips=[("Coin", "@coin"), (oscilator.upper(), hovertool_oscilator)]))
 
     p.add_layout(labels)
+    p.xaxis.major_label_orientation = 1.2
+
     
-    p.toolbar_location = None
+    if oscilator == 'BB.Position':
+
+        upper1 = BoxAnnotation(bottom=2, fill_alpha=0.2, fill_color='olive')
+        p.add_layout(upper1)
+
+        upper2 = BoxAnnotation(bottom=1, fill_alpha=0.1, fill_color='palegreen')
+        p.add_layout(upper2)
+
+        upper3 = BoxAnnotation(bottom=0, fill_alpha=0.1, fill_color='cornsilk')
+        p.add_layout(upper3)
+
+        lower1 = BoxAnnotation(top=-1, fill_alpha=0.1, fill_color='salmon')
+        p.add_layout(lower1)
+
+        lower2 = BoxAnnotation(top=-2, fill_alpha=0.2, fill_color='red')
+        p.add_layout(lower2)
+
+        lower3 = BoxAnnotation(top=0, fill_alpha=0.1, fill_color='cornsilk')
+        p.add_layout(lower3)
+
+        polovina = Span(location=0,
+                         line_color='orange',line_dash='dashed', line_width=2)
+        p.add_layout(polovina)
+
+        bb_up = Span(location=2,
+                         line_color='dodgerblue', line_dash='dashed', line_width=2)
+        p.add_layout(bb_up)
+
+        bb_down = Span(location=-2,
+                         line_color='dodgerblue',line_dash='dashed', line_width=2)
+        p.add_layout(bb_down)
+        
     
     return p
   
   
 
+def grafik_oscilator_interval_sp(dt,interval,oscilator,usdt_btc,sort=True):
+    
+    if sort:
+        dt = dt.sort_values(by=[oscilator])
+
+    source = ColumnDataSource(data=dict(
+        coin=dt['coin'], 
+        indikator = dt[oscilator],
+        names=dt['coin']))
+    
+    prosecan_oscilator = round(dt[oscilator].mean(),2)
+    mediana_oscilator = round(dt[oscilator].median(),2)
+    std_oscilator = round(dt[oscilator].std(),2)
+
+    ime_grafika_osnovno = oscilator.upper() + ' ' + interval + ' ' + usdt_btc 
+    ime_nastavak = '\nMean: ' + str(prosecan_oscilator) + '  Median: ' + str(mediana_oscilator) + '  STD: ' + str(std_oscilator)
+    ime_grafika = ime_grafika_osnovno + ime_nastavak
+ 
+    p = figure(y_range=dt['coin'],x_range =(min(dt[oscilator])*1.05,max(dt[oscilator])*1.05),#height=600,width=1200,  
+               title = ime_grafika, toolbar_location=None)
+
+    p.title.align = 'center'
+
+    p.sizing_mode = 'scale_height'
+#     "fixed", "stretch_both", "scale_width", "scale_height", "scale_both"
+    
+    
+
+
+    palette = RdYlGn[10]
+    color_mapper = linear_cmap(field_name = 'indikator', 
+                               palette = palette, low = dt[oscilator].max(), 
+                               high = dt[oscilator].min())
+
+    p.scatter(y='coin', x='indikator', size=7, source=source, fill_color=color_mapper)
+    
+    p.xaxis.axis_label = oscilator.upper() 
+    p.yaxis.axis_label = 'Coin'
+
+    labels = LabelSet(y='coin', x='indikator', 
+                      text='names',text_alpha = 0.9,text_font_size = '5pt',
+                      text_align = 'left',
+                      level='glyph',
+                      x_offset = 6, y_offset = -3,
+                      source=source)
+
+    p.add_layout(labels)
+#     p.xaxis.major_label_orientation = 1.2
+
+    
+    if oscilator == 'BB.Position':
+
+        upper1 = BoxAnnotation(left=2, fill_alpha=0.2, fill_color='olive')
+        p.add_layout(upper1)
+
+        upper2 = BoxAnnotation(left=1, fill_alpha=0.1, fill_color='palegreen')
+        p.add_layout(upper2)
+
+        upper3 = BoxAnnotation(left=0, fill_alpha=0.1, fill_color='cornsilk')
+        p.add_layout(upper3)
+
+        lower1 = BoxAnnotation(right=-1, fill_alpha=0.1, fill_color='salmon')
+        p.add_layout(lower1)
+
+        lower2 = BoxAnnotation(right=-2, fill_alpha=0.2, fill_color='red')
+        p.add_layout(lower2)
+
+        lower3 = BoxAnnotation(right=0, fill_alpha=0.1, fill_color='cornsilk')
+        p.add_layout(lower3)
+
+        polovina = Span(location=0,dimension='height',
+                         line_color='orange',line_dash='dashed', line_width=2)
+        p.add_layout(polovina)
+
+        bb_up = Span(location=2, dimension='height',
+                         line_color='dodgerblue', line_dash='dashed', line_width=2)
+        p.add_layout(bb_up)
+
+        bb_down = Span(location=-2,dimension='height',
+                         line_color='dodgerblue',line_dash='dashed', line_width=2)
+        p.add_layout(bb_down)
+
+    return p  
+  
+  
+  
 
 ponudjeni_simboli =  ['RLC', 'PEPE','ID','JOE', '1INCH', 'AAVE', 'ACM', 'ADA', 'AGIX', 'AKRO', 'ALGO', 'ALICE', 'ALPHA','APT','APE', 'ANKR', 'ANT',
                       'ARB', 'ARDR', 'ARPA', 'ASR', 'ATM', 'ATOM', 'AUDIO', 'AVAX', 'AXS', 
                       'BADGER', 'BAL', 'BAND', 'BAT', 'BCH', 'BEL', 'BLZ', 'BNB',  'BTC', 
-                      'BURGER', 'CAKE', 'CELO', 'CELR', 'CFX', 'CHR', 'CHZ', 'CKB', 'COCOS', 
+                      'BURGER', 'CAKE', 'CELO', 'CELR', 'CFX', 'CHR', 'CHZ', 'CKB', 
                       'COMP', 'COTI', 'CRV', 'CTK', 'CTSI', 'CTXC', 'CVC', 'CVX', 'DASH', 'DATA', 'DCR', 'DEGO',
                       'DENT', 'DGB', 'DIA', 'DOCK', 'DODO', 'DOGE', 'DOT', 'DREP', 'DUSK', 'DYDX', 'EGLD',
                       'ENJ', 'EOS', 'ETC', 'ETH', 'FET', 'FIL', 'FIO', 'FIRO', 'FIS', 'FLM','FLOW', 'FTM', 
@@ -163,9 +281,9 @@ ponudjeni_intervali = ["1m", "5m", "15m", "30m", "1h", "2h", "4h", "1d","1W", "1
 
 ponudjeni_parovi = ["USDT", "BTC"]
 
-sortiranje_ponuda = ['rsi','coin']
+sortiranje_ponuda = ['value','coin']
 
-oscilator = 'rsi'
+# oscilator = 'rsi'
 
 
 
@@ -173,7 +291,7 @@ container = st.container()
 
 
 
-tab1, tab2 = container.tabs(["📈 Chart", "📋 Table"])
+tab1, tab2 = container.tabs(["📈 Smartphone format", "📋 PC format"])
 
 container.write("Feel free to leave a tip.")
 container.write("Eth: 0xb77fcef5c13e1a54bdfff9a7e5268743e81325c4")
@@ -197,7 +315,7 @@ if podesavanja_korisnika:
     
 else:
     pocetni_simboli = ['1INCH', 'AAVE', 'ADA', 'AGIX', 'ALGO', 'ANKR', 'APE', 'APT', 'ARB', 'ATOM', 'AUDIO', 'AVAX', 'BCH', 'BNB', 'BTC',
-                       'CAKE', 'CFX', 'CHZ', 'COCOS', 'CRV', 'CVX', 'DASH', 'DOGE', 'DOT', 'DYDX', 'EGLD', 'ENJ', 'EOS', 'ETC', 'ETH', 
+                       'CAKE', 'CFX', 'CHZ', 'CRV', 'CVX', 'DASH', 'DOGE', 'DOT', 'DYDX', 'EGLD', 'ENJ', 'EOS', 'ETC', 'ETH', 
                        'FET', 'FIL', 'FLOW', 'FTM', 'FXS', 'GALA', 'GMX', 'GRT', 'HBAR', 'HOOK', 'HOT', 'ICP', 'ID', 'IMX', 'INJ', 'IOTA', 'KAVA', 
                        'KLAY', 'LDO', 'LINK', 'LQTY', 'LTC', 'LUNC', 'MANA', 'MASK', 'MATIC', 'MINA', 'MKR', 'NEAR', 'NEO', 'NEXO', 
                        'OCEAN', 'OP', 'OSMO', 'PEPE', 'QNT', 'RNDR', 'ROSE', 'RPL', 'RUNE', 'RVN', 'SAND', 
@@ -216,7 +334,7 @@ sortiranje_ponuda_pocetni = 0
 with st.sidebar.form(key ='Form1'):
     
     
-    st.header('RSI `version 1`')
+    st.header('`Single TF Analyze`')
     
     st.form_submit_button(label = "Submit")
     
@@ -232,10 +350,10 @@ with st.sidebar.form(key ='Form1'):
     kolona_sortiranja = st.selectbox('Sort by', sortiranje_ponuda,sortiranje_ponuda_pocetni)
     
     
-    usdt_btc = st.multiselect('USDT and/or BTC',ponudjeni_parovi,ponudjeni_parovi[ponudjeni_parovi_pocetni])
+    usdt_btc = st.selectbox('USDT or BTC',ponudjeni_parovi,ponudjeni_parovi_pocetni)
     
     
-    chart_table = st.multiselect('Chart and/or Table', ['Chart','Table'],'Chart')
+#     chart_table = st.multiselect('Chart and/or Table', ['Chart','Table'],'Chart')
     
 
     podesavanja = simboli
@@ -249,17 +367,33 @@ if usdt_btc and kolona_sortiranja:
 
     for izbor_usdt_btc in usdt_btc:
       
-        dt = data_frame_maker(simboli, interval, oscilator, izbor_usdt_btc, kolona_sortiranja)
-        p = grafik_oscilator_interval(dt,interval,oscilator,izbor_usdt_btc)
-        dt = dt.set_index('coin')
-        dt.rsi = round(dt[['rsi']],1).astype(str)
-       
-        stil = dt.style.background_gradient(axis=0, cmap = 'RdYlGn' ,subset=['rsi'] )
-        if 'Chart' in chart_table:
-            with tab1:
-                tab1.bokeh_chart(p)
-        if 'Table' in chart_table:
-            with tab2:
-                tab2.dataframe(stil,use_container_width= True)
+        dt = data_frame_maker(simboli, interval, [ 'close','low','high','BB.upper','BB.lower','RSI','change'], izbor_usdt_btc, ['timeframe'])
+        
+        conditions = [
+        (dt['BB.upper'].isna() | dt['BB.lower'].isna() ),
+        ((dt['BB.upper']<= dt.high) | (dt['BB.upper']<= dt.close)),
+        ((dt['BB.lower']>= dt.low) | (dt['BB.lower']>=dt.close)),
+        ((dt['BB.upper']> dt.high) | (dt['BB.upper']> dt.close) | (dt['BB.lower']< dt.low) | (dt['BB.lower']<dt.close))
+        ]
+        
+        values = [np.nan, 'UP','LOW','']
+
+        dt['BB'] =np.select(conditions,values)
+        
+        dt['BB.SMA'] =  (dt['BB.upper'] + dt['BB.lower']) /2
+        dt['BB.STD'] = (dt['BB.upper'] - dt['BB.SMA']) /2
+        dt['BB.Position'] = round((dt['close'] - dt['BB.SMA']) / dt['BB.STD'],2)
+        
+        
+        with tab1:
+            if kolona_sortiranja == 'coin':
+              sortiranje = False
+            p_rsi_sp = grafik_oscilator_interval_sp(dt[['coin','RSI']],interval,'RSI',izbor_usdt_btc,sortiranje)
+            tab1.bokeh_chart(p)
+        
+        with tab2:
+            p_rsi_pc = grafik_oscilator_interval_pc(dt[['coin','RSI']],interval,'RSI',izbor_usdt_btc,sortiranje)
+            tab2.bokeh_chart(p)
+            
         
 container.download_button("Download Coin List",json_podesavanja,"my_coin_list.json","application/json")
