d6f12857180a89f126b01a69cf9f55486861a217
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-09 13:50:53 +0200

    Update Single Time Frame Analyze.py

--- pages/Single Time Frame Analyze.py
+++ pages/Single Time Frame Analyze.py
@@ -173,7 +173,7 @@ def grafik_oscilator_interval_pc(dt,interval,oscilator,usdt_btc,sort=True):
     
     now = datetime.now()
     datum_vreme = now.strftime("%H:%M %d/%m/%y")
-    ime_grafika_osnovno = ime_za_naslov + ' ' + interval + ' ' + usdt_btc + ' ' + datum_vreme
+    ime_grafika_osnovno = ime_za_naslov + ' ' + interval + ' ' + usdt_btc + '     ' + datum_vreme
     ime_nastavak = '\nMean: ' + str(prosecan_oscilator) + '  Median: ' + str(mediana_oscilator) + '  STD: ' + str(std_oscilator) + ' Min: ' + str(min_oscilatro) + ' Max: ' + str(max_oscilatro)
     ime_grafika = ime_grafika_osnovno + ime_nastavak
     
