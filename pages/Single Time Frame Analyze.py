77eb748cd054752078d1baf8655ed98b7388a857
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-10 20:25:08 +0200

    Update Single Time Frame Analyze.py

--- pages/Single Time Frame Analyze.py
+++ pages/Single Time Frame Analyze.py
@@ -176,15 +176,15 @@ def grafik_oscilator_interval_pc(dt,interval,oscilator,usdt_btc,sort=True):
           
   
     vrednost = dt[dt.coin == usdt_btc][oscilator].values[0]
-    vecih = str(round(dt[dt[oscilator]> vrednost][oscilator].count()/len(dt.index) *100,2)) + '%'
-    manjih = str(round(dt[dt[oscilator]< vrednost][oscilator].count()/len(dt.index) *100,2)) + '%'
+    vecih = str(round(dt[dt[oscilator]> vrednost][oscilator].count()/len(dt.index) *100,1)) + '%'
+    manjih = str(round(dt[dt[oscilator]< vrednost][oscilator].count()/len(dt.index) *100,1)) + '%'
     
     now = datetime.now() + timedelta(hours=2)
     datum_vreme = now.strftime("%H:%M %d/%m/%y") 
     ime_grafika_osnovno = ime_za_naslov + ' ' + interval + ' ' + usdt_btc + '     ' + datum_vreme
     ime_nastavak = '\nMean: ' + str(prosecan_oscilator) + '  Median: ' + str(mediana_oscilator) + \
     '  STD: ' + str(std_oscilator) + ' Min: ' + str(min_oscilator) + ' Max: ' + str(max_oscilator) + \
-    ' --- ' + usdt_btc + '>' + manjih + '   ' + usdt_btc + '<' + vecih
+    '     ' + usdt_btc + '>' + manjih + '   ' + usdt_btc + '<' + vecih
     ime_grafika = ime_grafika_osnovno + ime_nastavak
     
 
