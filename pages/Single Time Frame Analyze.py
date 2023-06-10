7cf496239f45964480e1aa148ad1cb11304f9aa7
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-10 02:50:44 +0200

    Update Single Time Frame Analyze.py

--- pages/Single Time Frame Analyze.py
+++ pages/Single Time Frame Analyze.py
@@ -400,10 +400,8 @@ def grafik_oscilator_interval_sp(dt,interval,oscilator,usdt_btc,sort=True):
         else:
             distanca = max(abs((max(dt[oscilator]) - min(dt[oscilator]))) *0.3 , 2)
         
-        x2 = max(dt[oscilator]) + distanca
-        x1 = min(dt[oscilator]) - distanca
-#         x2 = max(max(dt[oscilator]) + distanca, 105)
-#         x1 = min(min(dt[oscilator]) - distanca, -105)
+        x2 = max(max(dt[oscilator]) + distanca, 150)
+        x1 = min(min(dt[oscilator]) - distanca, -150)
 
  
     p = figure(y_range=dt['coin'],x_range =(x1,x2),#width=350,height=600,  
