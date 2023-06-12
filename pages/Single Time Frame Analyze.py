f9be38de605bfaab21f24f9da039b3c9f782cf0e
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-13 01:21:30 +0200

    Update Single Time Frame Analyze.py

--- pages/Single Time Frame Analyze.py
+++ pages/Single Time Frame Analyze.py
@@ -549,7 +549,7 @@ def grafik_oscilator_interval_sp(dt,interval,oscilator,usdt_btc,sort=True):
     
     
     if usdt_btc in list(dt.coin):
-        coin_location = Span(location= dt[dt.coin == usdt_btc][oscilator].values[0],
+        coin_location = Span(location= dt[dt.coin == usdt_btc][oscilator].values[0],dimension='height',
                              line_color='magenta',line_dash='solid', line_width=1)
         p.add_layout(coin_location)
         
