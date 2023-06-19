7236cf72f6d7e4f14b69b9725aa7c479ca14b5f3
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-19 21:12:46 +0200

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config( page_title = "Crypto Data", page_icon = "📊",layout="wide",initial_sidebar_state="auto", menu_items = {"About": "krsticsmederevac@gmail.com"})
 
 import extra_streamlit_components as stx
-from streamlit_extras.app_logo import add_logo
+# from streamlit_extras.app_logo import add_logo
 
 
 url1 = "https://www.tradingview.com/script/48BzLTIt-CryptoData-Coin-BTC-Quick-Chart-ic/"
