36444aa97bf63361e779c880ec0e6b692b5418fa
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-06-20 13:52:57 +0200

    Update RSI Filter.py

--- pages/RSI Filter.py
+++ pages/RSI Filter.py
@@ -466,7 +466,7 @@ with st.sidebar.form(key ='Form1'):
     
     st.form_submit_button(label = "Submit")
     
-    rsi_range = st.slider('RSI Value Between', min_value=1, max_value=100, value=(40,60), step=1)
+    rsi_range = st.slider('RSI Value Between', min_value=0, max_value=100, value=(40,60), step=1)
     rsi_inverse = st.checkbox('Inverse Range',value = True)
   
     interval = st.selectbox('Time Frame', ponudjeni_intervali,ponudjeni_intervali_pocetni)
