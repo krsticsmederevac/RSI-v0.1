46664d1ad442d73260e82e9d21ef7e71a560a30b
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2023-07-27 14:57:33 +0200

    Update One Coin Analyze.py

--- pages/One Coin Analyze.py
+++ pages/One Coin Analyze.py
@@ -197,7 +197,7 @@ def data_frame_maker(simboli, intervali, analitike, usdt_btc, kolona_sortiranja)
 
 container = st.container()
 
-tab1 = container.tabs(["📋 Analyze",'ss']) 
+tab1, tab2 = container.tabs(["📋 Analyze",'ss']) 
 
 
 
