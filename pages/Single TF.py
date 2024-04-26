924cb4f05c43375aa502d8e2fb97a60acc679686
krsticsmederevac <50680409+krsticsmederevac@users.noreply.github.com>
2024-04-26 20:31:47 +0200

    Update Single TF.py

--- pages/Single TF.py
+++ pages/Single TF.py
@@ -1123,7 +1123,7 @@ with st.sidebar.form(key ='Form1'):
     
 if usdt_btc and kolona_sortiranja:
 
-  for i in range(5):
+  while True:
   
 
     
@@ -1273,6 +1273,6 @@ if usdt_btc and kolona_sortiranja:
              tab2.bokeh_chart(p_sma50_pc)
          except: 
              print()
-     time.sleep(10)
+     time.sleep(20)
 
 container.download_button("Download Coin List",json_podesavanja,"my_coin_list.json","application/json")
